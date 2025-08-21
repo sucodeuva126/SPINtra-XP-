e0af59b1e18801090cf086e640063f4f1c14288e
Gustavo <gus.henrique126@usp.br>
2025-08-21 16:14:47 -0300

    com bvt server is now grouped inside a function

--- bin/bvt_functions.py
+++ bin/bvt_functions.py
@@ -6,16 +6,18 @@ class BVT:
     self.isTemperatureReady = False
     self.current_temp = None
     self.threshold = threshold
-    self.bvt_server = win.Dispatch("WinAcquisit.BVT")
-    self.emb = win.Dispatch("WinAcquisit.Embedding")
-    self.emb.ShowWindow(self.emb.NORMAL)
-    self.uti = win.Dispatch("WinAcquisit.Utilities")
+  
+  def init_com_server(self): #the COM object is not thread safe, so it needs to be initialized here again
+    if not hasattr(self, 'emb'):
+      self.emb = win.Dispatch("WinAcquisit.Embedding")
+      self.emb.ShowWindow(self.emb.NORMAL)
+    if not hasattr(self, 'bvt_server'):
+      self.bvt_server = win.Dispatch("WinAcquisit.BVT")
+    if not hasattr(self, 'uti'):
+      self.uti = win.Dispatch("WinAcquisit.Utilities")
 
   def start(self, gas_flow, evaporator):
-    self.bvt_server = win.Dispatch("WinAcquisit.BVT") #the COM object is not thread safe, so it needs to be initialized here again
-    self.emb = win.Dispatch("WinAcquisit.Embedding")
-    self.emb.ShowWindow(self.emb.NORMAL)
-    self.uti = win.Dispatch("WinAcquisit.Utilities")
+    self.init_com_server()
     self.bvt_server.GasFlow(gas_flow)
     self.bvt_server.GasFlowOn(True)
     if evaporator:
@@ -25,19 +27,13 @@ class BVT:
     return
 
   def set_point_and_start_ramp(self, temp):
-    self.bvt_server = win.Dispatch("WinAcquisit.BVT") #the COM object is not thread safe, so it needs to be initialized here again
-    self.emb = win.Dispatch("WinAcquisit.Embedding")
-    self.emb.ShowWindow(self.emb.NORMAL)
-    self.uti = win.Dispatch("WinAcquisit.Utilities")
+    self.init_com_server()
     self.bvt_server.DesiredTemperature(temp)
     self.bvt_server.RampGO
     return 
 
   def autotune(self, switch):
-    self.bvt_server = win.Dispatch("WinAcquisit.BVT") #the COM object is not thread safe, so it needs to be initialized here again
-    self.emb = win.Dispatch("WinAcquisit.Embedding")
-    self.emb.ShowWindow(self.emb.NORMAL)
-    self.uti = win.Dispatch("WinAcquisit.Utilities")
+    self.init_com_server()
     if switch == True:
       self.bvt_server.PIDTuneOn(True)
     if switch == False:
@@ -45,20 +41,14 @@ class BVT:
     return
     
   def get_temperature(self):
-    self.bvt_server = win.Dispatch("WinAcquisit.BVT") #the COM object is not thread safe, so it needs to be initialized here again
-    self.emb = win.Dispatch("WinAcquisit.Embedding")
-    self.emb.ShowWindow(self.emb.NORMAL)
-    self.uti = win.Dispatch("WinAcquisit.Utilities")
+    self.init_com_server()
     try:
         self.current_temp  = self.bvt_server.GetTemperature #saves the temperature read
     except Exception as e:
         print("ERROR IN READING TEMPERATURES", e)
 
   def check_temperature(self, temp): #thread function
-    self.bvt_server = win.Dispatch("WinAcquisit.BVT") #the COM object is not thread safe, so it needs to be initialized here again
-    self.emb = win.Dispatch("WinAcquisit.Embedding")
-    self.emb.ShowWindow(self.emb.NORMAL)
-    self.uti = win.Dispatch("WinAcquisit.Utilities")
+    self.init_com_server()
     if self.current_temp is not None:
         if self.bvt_server.IsTemperatureOK: #verify if the mesured temperature is the desired temperature 
             self.isTemperatureReady = True
